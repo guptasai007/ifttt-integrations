2055a2b9ac7191800fe80295d9bc4fdc5bfd2e06
Abhishek Gupta <abhishek.gupta@birdeye.com>
2022-07-16 00:17:39 +0530

    Added webhook endpoint and logged body & headers

--- src/main/java/com/abhishekinformatics/iftttintegrations/controllers/IFTTTWebhookController.java
+++ src/main/java/com/abhishekinformatics/iftttintegrations/controllers/IFTTTWebhookController.java
@@ -2,7 +2,10 @@ package com.abhishekinformatics.iftttintegrations.controllers;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,8 +13,9 @@ public class IFTTTWebhookController {
 
 	Logger logger = LoggerFactory.getLogger(this.getClass());
 
-	@GetMapping("/hello")
-	public void hello() {
-		logger.info("hello endpoint hit");
+	@PostMapping("/webhook")
+	public void postEvent(@RequestBody String body, @RequestHeader MultiValueMap<String, String> headers) {
+		logger.info("body: {}", body);
+		logger.info("header: {}", headers);
 	}
 }
